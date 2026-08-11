5fbdfdf64f91efa74f62a36456f8aa5c7a5db595
Joonas <joonas.aapro@mainiotech.fi>
2026-08-11 11:10:14 +0300

    Upgrade to 0.31

--- config/assets.rb
+++ config/assets.rb
@@ -2,12 +2,12 @@
 
 base_path = File.expand_path("..", __dir__)
 
-Decidim::Webpacker.register_path("#{base_path}/app/packs", prepend: true)
-Decidim::Webpacker.register_entrypoints(
+Decidim::Shakapacker.register_path("#{base_path}/app/packs", prepend: true)
+Decidim::Shakapacker.register_entrypoints(
   decidim_budgets_booth_voting: "#{base_path}/app/packs/entrypoints/decidim_budgets_booth_voting.js",
   decidim_budgets_booth_zip_code: "#{base_path}/app/packs/entrypoints/decidim_budgets_booth_zip_code.js",
   decidim_handle_voting_complete: "#{base_path}/app/packs/entrypoints/decidim_handle_voting_complete.js",
   decidim_budgets_booth_show_projects: "#{base_path}/app/packs/entrypoints/decidim_budgets_booth_show_projects.js"
 )
 
-Decidim::Webpacker.register_stylesheet_import("stylesheets/decidim/budgets_booth/budgets_booth")
+Decidim::Shakapacker.register_stylesheet_import("stylesheets/decidim/budgets_booth/budgets_booth")
