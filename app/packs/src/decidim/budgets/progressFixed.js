9249dc137461445494745ffc7cc8d4fff38ede45
Antti Hukkanen <antti.hukkanen@mainiotech.fi>
2026-09-08 15:48:53 +0300

    Fix JS error
    Fix the following JS error:
    `Uncaught TypeError: Cannot set properties of undefined (setting 'toggleBackdrop')`

--- app/packs/src/decidim/budgets/progressFixed.js
+++ app/packs/src/decidim/budgets/progressFixed.js
@@ -1,3 +1,9 @@
 // We are customizing this functionality at decidim/budgets_booth/projects.js
 // which is why we have disabled the default functionality by overriding this
 // file.
+
+// This is still needed because `budgetSummaryBackdrop.js` assumes this has
+// already been set by this file.
+document.addEventListener("turbo:load", () => {
+  window.DecidimBudgets = window.DecidimBudgets || {};
+});
